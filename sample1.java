7b876b2b7e78f126d6dc4a53e286470b92ad3065
kitoumasaomi <kanfutrooper@gmail.com>
2025-11-15 15:07:58 +0900

    sample1.javaを作成する

--- sample1.java
+++ sample1.java
@@ -1,3 +1,9 @@
 class sample1 {
-    
+    // フィールド(クラス直下で定義された変数)
+    String carModel = "クーペ"; // 車種名
+    String owner = "モコ"; // オーナー
+    String color = "RED"; // 塗装色
+    int speed = 0; // 現在の速度
+    Boolean right = false; // ライト点灯(true:点灯/false:消灯)
+
 }
