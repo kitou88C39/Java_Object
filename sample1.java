4b763d1061dd4d572cc9c8c436c5799f9740c242
kitoumasaomi <kanfutrooper@gmail.com>
2025-11-15 15:14:39 +0900

    sample1.javaを作成する

--- sample1.java
+++ sample1.java
@@ -6,4 +6,34 @@ class sample1 {
     int speed = 0; // 現在の速度
     Boolean right = false; // ライト点灯(true:点灯/false:消灯)
 
+    // メソッド(クラス直下で定義された関数)
+    // acceleratorメソッド(現在の速度を1km/h上げる)
+    void accelerator() {
+        speed++;
+        System.out.println("(少し速くなった)");
+    }
+
+    // brakeメソッド(現在の速度を1km/h下げる)
+    void brake() {
+        speed--;
+        System.out.println("(少し遅くなった)");
+    }
+
+    // rightOnメソッド(ライトを点灯させる)
+    void rightOn() {
+        right = true;
+        System.out.println("(ライトを点灯させる)");
+    }
+
+    // rightOffメソッド(ライトを消灯させる)
+    void rightOff() {
+        right = false;
+        System.out.println("(ライトを消灯させる)");
+    }
+
+    // klaxonメソッド(クラクションを鳴らす)
+    void klaxon() {
+        System.out.println("(クラクションを鳴らす)");
+    }
+
 }
