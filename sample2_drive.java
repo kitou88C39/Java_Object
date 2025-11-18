cf0653a7a43f16b5c9ac500d99cd91d24dc4a726
kitoumasaomi <kanfutrooper@gmail.com>
2025-11-18 11:43:02 +0900

    sample2_drive.javaを作成する

--- sample2_drive.java
+++ sample2_drive.java
@@ -34,6 +34,36 @@ class sample2_drive {
         System.out.println("pochiCar.speed:" + pochiCar.speed);
         System.out.println("pochiCar.right:" + pochiCar.right);
 
+        System.out.println("----------");
+
+        // ③メソッドの起動＆確認
+        System.out.println("③メソッドの起動＆確認----------");
+
+        chocoCar.accelerator(); // chocoCarはacceleratorメソッド起動
+        mocoCar.rightOff(); // mocoCarはrightOffメソッド起動
+        pochiCar.brake(); // pochiCarはbrakeメソッド起動
+
+        System.out.println("<chocoCar>");
+        System.out.println("chocoCar.carModel:" + chocoCar.carModel);
+        System.out.println("chocoCar.owner:" + chocoCar.owner);
+        System.out.println("chocoCar.color:" + chocoCar.color);
+        System.out.println("chocoCar.speed:" + chocoCar.speed);
+        System.out.println("chocoCar.right:" + chocoCar.right);
+
+        System.out.println("<mocoCar>");
+        System.out.println("mocoCar.carModel:" + mocoCar.carModel);
+        System.out.println("mocoCar.owner:" + mocoCar.owner);
+        System.out.println("mocoCar.color:" + mocoCar.color);
+        System.out.println("mocoCar.speed:" + mocoCar.speed);
+        System.out.println("mocoCar.right:" + mocoCar.right);
+
+        System.out.println("<pochiCar>");
+        System.out.println("pochiCar.carModel:" + pochiCar.carModel);
+        System.out.println("pochiCar.owner:" + pochiCar.owner);
+        System.out.println("pochiCar.color:" + pochiCar.color);
+        System.out.println("pochiCar.speed:" + pochiCar.speed);
+        System.out.println("pochiCar.right:" + pochiCar.right);
+
     }
 
 }
