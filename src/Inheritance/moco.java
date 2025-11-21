2e4feb968deb67f653c06f8f156dd8c0c26d47ac
kitoumasaomi <kanfutrooper@gmail.com>
2025-11-21 17:12:19 +0900

    moco.javaを作成する

--- src/Inheritance/moco.java
+++ src/Inheritance/moco.java
@@ -1,5 +1,13 @@
 package src.Inheritance;
 
-public class moco {
+public class moco extends dogs {
 
+    // フィールド
+    String type = "犬科";
+
+    // メソッド
+    // ストレッチ
+    void stretch() {
+        System.out.println("ストレッチ");
+    }
 }
