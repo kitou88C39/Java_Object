6d898fcb185a442b29e7475db301806f7b57e759
kitoumasaomi <kanfutrooper@gmail.com>
2025-11-21 17:02:21 +0900

    animals.javaを作成する

--- src/Inheritance/animals.java
+++ src/Inheritance/animals.java
@@ -2,4 +2,12 @@ package src.Inheritance;
 
 public class animals {
 
+    // フィールド
+    String group = "哺乳類";
+
+    // メソッド
+    // 肺呼吸
+    void breatheLungs() {
+        System.out.println("肺で呼吸");
+    }
 }
