7de9b348890a48db63fda2a33632a02997d7edd4
kitoumasaomi <kanfutrooper@gmail.com>
2025-11-22 16:46:14 +0900

    Main.javaを作成する

--- src/Inheritance/Main.java
+++ src/Inheritance/Main.java
@@ -1,5 +1,20 @@
 package src.Inheritance;
 
-public class Main {
+class Main {
+    public static void main(String[] args) {
+        ToyPoodle toyPoo1 = new ToyPoodle();
+        ToyPoodle toyPoo2 = new ToyPoodle("キャンキャン");
+        ToyPoodle moco = new ToyPoodle("MOCO", "ストレッチ", "わんわん");
+
+        System.out.println("toyPoo1インスタンスの確認");
+        System.out.println(toyPoo1.name);
+        toyPoo1.bark();
+        toyPoo1.doSkill();
+
+        System.out.println("toyPoo2インスタンスの確認");
+        System.out.println(toyPoo2.name);
+        toyPoo2.bark();
+        toyPood2.doSkill();
+    }
 
 }
