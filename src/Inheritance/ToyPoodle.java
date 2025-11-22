658a9169bf03de9577b1896406d169812107705f
kitoumasaomi <kanfutrooper@gmail.com>
2025-11-22 16:06:18 +0900

    ToyPoodle.javaを作成する

--- src/Inheritance/ToyPoodle.java
+++ src/Inheritance/ToyPoodle.java
@@ -1,5 +1,20 @@
 package src.Inheritance;
 
-public class ToyPoodle {
+public class ToyPoodle extends Dog {
+
+    // フィールド
+    String name; // 名前
+    String skill; // 技能
+
+    // コンストラクタ
+    public ToyPoodle() {
+        this.name = "未設定(name)";
+        this.skill = "未設定(skill)";
+    }
+
+    public ToyPoodle(String v) {
+        this.name = "未設定(name)";
+        this.skill = "未設定(skill)";
+    }
 
 }
