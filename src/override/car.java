abc6b881bbc5b279533729f78999d832ad7d3f41
kitoumasaomi <kanfutrooper@gmail.com>
2025-11-22 20:19:13 +0900

    override/car.javaを作成する

--- src/override/car.java
+++ src/override/car.java
@@ -1,5 +1,16 @@
 package src.override;
 
 public class car {
+    // フィールド
+    protected String color; // 塗装色
+    private int speed; // 現在の速度
+
+    // コンストラクタ
+    // コンストラクタ①(引数無)
+    public Car(){
+        this.color = "未登録"; //塗装色
+        this.speed = 0; //現在の速度
+    
+    }
 
 }
