f34deadda838b241e9424686c066caf5cd73a717
kitoumasaomi <kanfutrooper@gmail.com>
2025-11-22 20:29:58 +0900

    override/car.javaを作成する

--- src/override/car.java
+++ src/override/car.java
@@ -7,10 +7,22 @@ public class car {
 
     // コンストラクタ
     // コンストラクタ①(引数無)
-    public Car(){
-        this.color = "未登録"; //塗装色
-        this.speed = 0; //現在の速度
+    public car() {
+        this.color = "未登録"; // 塗装色
+        this.speed = 0; // 現在の速度
     
+    //メソッド
+    //paintメソッド(色を塗る)※セットできる塗装色は色は『白』『黒』『赤』
+    public void paint(String color) {
+        if (color == "白" || color == "黒" || color == "赤") {
+            this.color = color;
+        }
     }
 
+    // acceleratorメソッド(現在の速度1km/h上げる)※180km/hにはならないように制御
+    public void accelerator() {
+        if (this.speed < 180) {
+            this.speed += 1;
+        }
+    }
 }
