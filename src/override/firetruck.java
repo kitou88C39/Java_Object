02ec195782c9d7c17d2840b21c6d44279806b6d8
kitoumasaomi <kanfutrooper@gmail.com>
2025-11-23 14:12:19 +0900

    firetruck.javaを作成する

--- src/override/firetruck.java
+++ src/override/firetruck.java
@@ -1,5 +1,25 @@
 package src.override;
 
-public class firetruck {
+public class firetruck extends car {
 
+    // フィールド
+    // ※なし
+
+    // コンストラクタ
+    // ※なし
+
+    // メソッド
+    // paintメソッド(色を塗る)※セットできる塗装色『赤』のみ
+    public void paint(String color) {
+        if (color.equals("赤")) {
+            super.color = color;
+        }
+    }
+
+    // acceleratorメソッド(現在の速度を1km/h上げる)※80km/h以上にならないように制御
+    public void accelerator() {
+        if (super.getSpeed() < 80) {
+            super.setSpeed(super.getSpeed() + 1);
+        }
+    }
 }
