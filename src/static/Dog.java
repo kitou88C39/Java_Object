7a9bcf85be7ddd5304f3804b0b529a03f744a263
kitoumasaomi <kanfutrooper@gmail.com>
2025-11-24 15:18:53 +0900

    static/Dog.javaを作成する

--- src/static/Dog.java
+++ src/static/Dog.java
@@ -1,5 +1,15 @@
 package src.static;
 
-public class Dog {
+class Dog {
+    //定数
+    public static final SCIENTIFIC_NAME = "Canis familiaris"; //学部名: カニスファミリアーリス
+
+    //フィールド
+    public int dogNumber = 0;
+    public String breed;
+    public String name;
+    
+    
+
     
 }
