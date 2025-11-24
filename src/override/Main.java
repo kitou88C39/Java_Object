8120ad2097e08d3fc8537e48d435e0cd52161f9f
kitoumasaomi <kanfutrooper@gmail.com>
2025-11-24 14:48:48 +0900

    Main.javaを作成する

--- src/override/Main.java
+++ src/override/Main.java
@@ -1,5 +1,19 @@
 package src.override;
 
-public class Main {
+class Main {
+    public static void main(String[] args) {
 
+        // ①インスタンスを生成
+        toyPoodle toyPoodle = new toyPoodle("MOCO", "ストレッチ");
+        goldenRetriverer goldenRetriverer = new goldenRetriverer("POCHI", "大食い");
+
+        // ②toStringメソッドの動作確認
+        System.out.println("toStringメソッドの動作確認------");
+        System.out.println(moco);
+
+        // ③toStringメソッドの起動
+        System.out.println();
+        System.out.println("toStringメソッドの動作確認------");
+        System.out.println(pochi);
+    }
 }
