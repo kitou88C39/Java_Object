f1cb72921d3562a08f908e0dff648dd7bc185e0e
kitoumasaomi <kanfutrooper@gmail.com>
2025-11-24 14:33:13 +0900

    toyPoodle.javaを作成する

--- src/override/toyPoodle.java
+++ src/override/toyPoodle.java
@@ -1,5 +1,18 @@
 package src.override;
 
 public class toyPoodle {
+    // フィールド
+    String name; // 名前
+    String skill; // 特技
 
+    // コンストラクタ
+    public toyPoodle(String name, String skill) {
+        this.name = name;
+        this.skill = skill;
+    }
+
+    // toStringメソッド(オーバーライド)
+    public String toString() {
+        return "toyPoodle(name:" + this.name + "/" + "skill:" + this.skill + ")";
+    }
 }
