aa475207a2516cf20f9ec73dbf581ed0838429c2
kitoumasaomi <kanfutrooper@gmail.com>
2025-11-25 20:23:15 +0900

    AbstractTruck.javaを作成する

--- src/abstract/AbstractTruck.java
+++ src/abstract/AbstractTruck.java
@@ -1,5 +1,12 @@
 package src.abstract;
 
-public class AbstractTruck {
+abstract public class AbstractTruck {
+    //抽象メソッド
+    //acceleratorメソッド(現在の速度を上げる)
+    public abstract void accelerator();
+    //brakeメソッド(現在の速度を下げる)
+    public abstract void brake();
+    //paintメソッド(色を塗る)
+    abstract void paint(String color);
     
 }
