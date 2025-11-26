ec5170482c5f4bca13c6326ba141f3ab63e845fe
kitoumasaomi <kanfutrooper@gmail.com>
2025-11-26 14:53:43 +0900

    Calc.javaを作成する

--- src/exceptionhandling/Calc.java
+++ src/exceptionhandling/Calc.java
@@ -1,5 +1,15 @@
 package src.exceptionhandling;
 
 public class Calc {
+    void calc(int x, int y) {
 
+        try {
+            System.out.println(x / y);
+        } catch (ArithmeticException e) {
+            System.out.println("[ERROR]例外を検知しました");
+            e.printStackTrace();
+        } finally {
+            System.out.println("finally");
+        }
+    }
 }
