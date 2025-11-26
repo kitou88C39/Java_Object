dc423146548800cd96acf9f97c7826293662b151
kitoumasaomi <kanfutrooper@gmail.com>
2025-11-26 11:21:12 +0900

    sample1.javaを作成する

--- src/exceptionhandling/sample1.java
+++ src/exceptionhandling/sample1.java
@@ -1,5 +1,13 @@
 package src.exceptionhandling;
 
-public class sample1 {
+public class Sample1 {
+    public static void main(String[] args) {
+    
+        int x = Integer.parseInt(args[0]);
+        int y = Integer.parseInt(args[1]);
 
+        System.out.println(x/y);
+        System.out.println("最後まで処理しました");
+        
+    }
 }
