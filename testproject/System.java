aeea624af230e3c0e26eda02cdddb34a7a119a09
kitoumasaomi <kanfutrooper@gmail.com>
2025-12-05 11:26:18 +0900

    System.java を作成

--- testproject/System.java
+++ testproject/System.java
@@ -2,4 +2,25 @@ package testproject;
 
 public class System {
 
+    @Component
+    public class ComponentPattern {
+        public String getText() {
+            return "This is Component";
+        }
+    }
+
+    @Configuration
+    public class ConfigurationPattern {
+        @Bean
+        public BeanClass getBeanClass() {
+            return new "This is Configuration";
+        }
+    }
+
+    class BeanClass {
+        public String getText() {
+            return "This is Bean";
+        }
+    }
+
 }
