c949580793d0728a9919b07347edaa953c54cb91
kitoumasaomi <kanfutrooper@gmail.com>
2025-12-05 10:30:29 +0900

    Application.java を作成

--- testproject/Application.java
+++ testproject/Application.java
@@ -2,4 +2,15 @@ package testproject;
 
 public class Application {
 
+    @controller
+    public class ContorllerPattern {
+        @Autowired
+        private ComponentPattern component;
+
+        @GetMapping("/Controller")
+        public String mainMethod() {
+            return component.getText;
+        }
+    }
+
 }
