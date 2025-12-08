ce77214f4a401cb9515547a94fcc79562a2f7ff0
kitoumasaomi <kanfutrooper@gmail.com>
2025-12-08 19:41:37 +0900

    MainController.java を作成

--- annotation/MainController.java
+++ annotation/MainController.java
@@ -1,3 +1,17 @@
+package com.annotation;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
 public class MainController {
+
+    @Value("${test-project.value1:default}")
+    private String propertyValue;
+
+    @GetMapping("/")
+    public String mainMethod() {
+        return propertyValue;
+    }
 
 }
