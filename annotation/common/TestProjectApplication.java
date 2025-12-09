a6faa020ee22639719cf6aaefa572a51557fd30f
kitoumasaomi <kanfutrooper@gmail.com>
2025-12-09 16:27:23 +0900

    TestProjectApplication.java を作成

---
+++ annotation/common/TestProjectApplication.java
@@ -0,0 +1,11 @@
+package annotation.common;
+
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class TestProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestProjectApplication.class, args);
+    }
+}
