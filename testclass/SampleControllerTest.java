19e0b3cc1bf7f3edfca8fe60cef0c009c19aaf69
kitoumasaomi <kanfutrooper@gmail.com>
2025-12-11 14:28:46 +0900

    SampleControllerTest.java を作成

--- testclass/SampleControllerTest.java
+++ testclass/SampleControllerTest.java
@@ -1,5 +1,20 @@
 package testclass;
 
+import java.beans.Transient;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class SampleControllerTest {
 
+    // Spring MVCを呼び出すモック
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testMainMethod() throws Exception {
+
+        this.mockMvc.perform(get("/sample"))// テスト実行
+                .andExpect(status().isOK())// ステータス評価
+                .andExpect(content().string("テスト1"));// 戻り値検証
+    }
 }
