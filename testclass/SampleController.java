ecf2404113bbbea7fc1d884b2bd22df21af9614e
kitoumasaomi <kanfutrooper@gmail.com>
2025-12-11 14:22:12 +0900

    SampleController.java を作成

--- testclass/SampleController.java
+++ testclass/SampleController.java
@@ -1,5 +1,13 @@
 package testclass;
 
+@RestController
 public class SampleController {
 
+    @Value("${test-project.value1}")
+    private String propertyValue;
+
+    @GetMapping("/sample")
+    public String mainMethod() {
+        return propertyValue;
+    }
 }
